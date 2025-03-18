00c1f61975e3257c0db8dbedb7b84e6cce8c3f54
issdn <karol.bielski@gmx.de>
2025-03-18 17:57:53 +0100

    switch from nodebuffer

--- office_image_extract.ts
+++ office_image_extract.ts
@@ -1,18 +1,17 @@
 import * as fs from 'node:fs';
 import JSZip from 'jszip';
 import { createWorker } from 'tesseract.js';
-import { Buffer } from 'node:buffer';
 
 export async function officeImageExtract(filepath: string) {
   const zip = new JSZip();
   const fileData = await fs.readFileSync(filepath);
   const docx = await zip.loadAsync(fileData);
 
-  const images: { name: string; data: Buffer }[] = [];
+  const images: { name: string; data: ArrayBuffer }[] = [];
 
   for (const fileName of Object.keys(docx.files)) {
     if (fileName.startsWith('word/media/')) {
-      const fileData = await docx.files[fileName].async('nodebuffer');
+      const fileData = await docx.files[fileName].async('arraybuffer');
       images.push({
         name: fileName.replace('word/media/', ''),
         data: fileData,
